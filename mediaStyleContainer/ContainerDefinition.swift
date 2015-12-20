e372fd25117641110ac3decaf8644aff2807930d
Yuka Ezura <yuka.ezura@gmail.com>
2015-12-20 20:49:11 +0900

    インタフェースを変えないで良い方法
    プロジェクト内で returnValue() をどのように使っているのかは見ていないため、今回この実装が良いかわかりませんので、プルリクを無視しても大丈夫ですー！

--- mediaStyleContainer/ContainerDefinition.swift
+++ mediaStyleContainer/ContainerDefinition.swift
@@ -6,35 +6,11 @@
 //  Copyright © 2015年 just1factory. All rights reserved.
 //
 
-//ContainerViewの識別用enum
-enum ContainerDefinition {
-    
-    //コンテナ番号の名称
-    case FirstContainer
-    case SecondContainer
-    case ThirdContainer
-    case FourthContainer
-    case FifthContainer
-    case SixthContainer
+enum ContainerDefinition: Int {
+    case FirstContainer, SecondContainer, ThirdContainer, FourthContainer, FifthContainer ,SixthContainer
     
     //状態対応するの値を返す
     func returnValue() -> Int {
-        
-        //各々のケース
-        switch (self) {
-            case .FirstContainer:
-                return 0
-            case .SecondContainer:
-                return 1
-            case .ThirdContainer:
-                return 2
-            case .FourthContainer:
-                return 3
-            case .FifthContainer:
-                return 4
-            case .SixthContainer:
-                return 5
-        }
+        return self.rawValue
     }
-    
 }
